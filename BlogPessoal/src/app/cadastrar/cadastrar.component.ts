01b6d2cb2bd600ca1d8206d0b141503a9ee1c10b
LPaifar <lucaspaifar1996@gmail.com>
2021-03-23 16:48:03 -0300

    implementação do component cadastro

--- BlogPessoal/src/app/cadastrar/cadastrar.component.ts
+++ BlogPessoal/src/app/cadastrar/cadastrar.component.ts
@@ -1,15 +1,54 @@
+import { AuthService } from './../service/auth.service';
 import { Component, OnInit } from '@angular/core';
+import { User } from '../model/User';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-cadastrar',
   templateUrl: './cadastrar.component.html',
   styleUrls: ['./cadastrar.component.css']
 })
-export class CadastrarComponent implements OnInit {
+export class CadastrarComponent implements OnInit
+{
+  user: User = new User
+  confirmarSenha: string
+  tipoUsuario: string
 
-  constructor() { }
+  constructor(
+    private authService: AuthService,
+    private router: Router )
+    {}
 
-  ngOnInit(): void {
+  ngOnInit()
+  {
+    window.scroll (0,0)
+  }
+
+  confirmSenha(event: any)
+  {
+    this.confirmarSenha = event.target.value
+  }
+
+  tipoUser(event: any)
+  {
+    this.tipoUsuario = event.target.value
+  }
+
+  cadastrar()
+  {
+    this.user.tipo = this.tipoUsuario
+
+    if(this.user.senha != this.confirmarSenha)
+    {
+      alert('As senhas estão incorretas.')
+    } else
+    {
+      this.authService.cadastrar(this.user).subscribe((resp: User) => {
+        this.user = resp
+        this.router.navigate(['/entrar'])
+        alert('Usuario cadastrado com sucesso!')
+      })
+    }
   }
 
 }

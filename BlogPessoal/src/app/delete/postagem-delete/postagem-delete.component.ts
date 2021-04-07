dfdcfa6db0676ad86a9cb62d9566a71494145577
LPaifar <lucaspaifar1996@gmail.com>
2021-04-07 09:27:15 -0300

    Implementando o Put e Delete em Postagem

--- BlogPessoal/src/app/delete/postagem-delete/postagem-delete.component.ts
+++ BlogPessoal/src/app/delete/postagem-delete/postagem-delete.component.ts
@@ -1,8 +1,7 @@
+import { PostagemService } from './../../service/postagem.service';
+import { Postagem } from './../../model/Postagem';
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
-import { Postagem } from 'src/app/model/Postagem';
-import { PostagemService } from 'src/app/service/postagem.service';
-import { TemaService } from 'src/app/service/tema.service';
 import { environment } from 'src/environments/environment.prod';
 
 @Component({
@@ -22,7 +21,6 @@ export class PostagemDeleteComponent implements OnInit {
   ) { }
 
   ngOnInit() {
-
     window.scroll(0,0)
 
     if(environment.token == ''){
@@ -34,16 +32,17 @@ export class PostagemDeleteComponent implements OnInit {
   }
 
   findByIdPostagem(id: number){
-    this.postagemService.getByIdPostagem(id).subscribe((resp: Postagem)=> {
+    this.postagemService.getByIdPostagem(id).subscribe((resp: Postagem)=>{
       this.postagem = resp
     })
   }
 
+
   apagar(){
-   this.postagemService.deletePostagem(this.idPost).subscribe(()=> {
-     alert('Post excluído com sucesso!')
-     this.router.navigate(['/inicio'])
-   })
+    this.postagemService.deletePostagem(this.idPost).subscribe(()=>{
+      alert('Postagem apagada com sucesso!')
+      this.router.navigate(['/inicio'])
+    })
   }
 
 }
